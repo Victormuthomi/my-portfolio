3dce554b3a837d6b974d7dffa1880c447c7cbfe2
muthomi100 <techeasesolns@gmail.com>
2025-02-01 19:14:11 +0300

    Redesign contact me component and footer component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,7 @@
-import React from "react";
 import { useForm, ValidationError } from "@formspree/react";
+import { MdConnectWithoutContact } from "react-icons/md";
+import { FaMessage, FaUser } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
 
 function ContactForm() {
   const [state, handleSubmit] = useForm("xqakodrl");
@@ -13,65 +15,84 @@ function ContactForm() {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="max-w-2xl mx-auto bg-slate-700 border px-6 py-4 rounded-lg shadow-lg "
-    >
-      <label htmlFor="name" className="block text-gray-700 mb-2">
-        Name
-      </label>
-      <input
-        id="name"
-        type="name"
-        name="name"
-        placeholder="Enter your Name"
-        className="w-full p-3 border bg-slate-900 rounded-lg mb-4"
-        required
-      />
-      <ValidationError prefix="Name" field="Name" errors={state.errors} />
-
-      <label htmlFor="email" className="block text-gray-700 mb-2">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email"
-        name="email"
-        placeholder="Enter your email"
-        className="w-full p-3 border bg-slate-900 rounded-lg mb-4"
-        required
-      />
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
-
-      <label htmlFor="message" className="block text-gray-700 mb-2">
-        Message
-      </label>
-      <textarea
-        id="message"
-        name="message"
-        placeholder="Enter your message"
-        className="w-full p-3 border bg-slate-900 rounded-lg mb-4"
-        required
-      />
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
-
-      <button
-        type="submit"
-        className="w-full bg-indigo-600 text-white py-3 rounded-lg hover:bg-indigo-700"
-        disabled={state.submitting}
+    <div className="flex justify-center items-center">
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-2xl w-[900px] border-4 border-fuchsia-300 px-6 py-4 rounded-3xl"
       >
-        {state.submitting ? "Sending..." : "Submit"}
-      </button>
-    </form>
+        <div className="relative">
+          <FaUser className="absolute mt-6 text-white text-2xl  transform -translate-y-1/2" />
+
+          <input
+            id="name"
+            type="name"
+            name="name"
+            placeholder="Name"
+            className="bg-transparent pl-10 w-full p-3 border-b-4 border-fuchsia-300 focus:border-fuchsia-500 outline-none placeholder:text-slate-100 text-2xl mb-8"
+            required
+          />
+        </div>
+        <ValidationError prefix="Name" field="Name" errors={state.errors} />
+
+        <div className="relative">
+          <MdEmail className="absolute mt-6 text-white text-2xl  transform -translate-y-1/2" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="bg-transparent pl-10 w-full p-3 border-b-4 border-fuchsia-300 focus:border-fuchsia-500 outline-none placeholder:text-slate-100 text-2xl mb-8"
+            required
+          />
+        </div>
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+
+        <div className="relative">
+          <FaMessage className="absolute text-white mt-6 ml- text-2xl  transform -translate-y-1/2" />
+
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Message"
+            className="bg-transparent pl-10 w-full p-3 border-b-4 border-fuchsia-300 focus:border-fuchsia-500 outline-none placeholder:text-slate-100 text-2xl mb-6"
+            required
+          />
+        </div>
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+
+        <div className="flex justify-center items-center">
+          <button
+            type="submit"
+            className=" bg-fuchsia-500 hover:bg-fuchsia-300 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg sm:w-auto"
+            disabled={state.submitting}
+          >
+            {state.submitting ? "Sending..." : "Submit"}
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
 function App() {
   return (
-    <div className="text-white py-20" id="contact">
-      <h2 className="text-4xl font-bold text-center mb-6">Contact Me</h2>
+    <>
+      <div
+        className="flex justify-center items-center underline decoration-fuchsia-300"
+        id="contact"
+      >
+        <h2 className=" text-5xl font-bold text-center text-fuchsia-500 mb-6">
+          CONTACT ME
+        </h2>
+        <MdConnectWithoutContact className="text-6xl ml-4 md:mb-6 mb-20 md:mt-0 mt-8 text-white " />
+      </div>
+
       <ContactForm />
-    </div>
+    </>
   );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       {/* <!-- Footer --> */}
-      <footer className="bg-gray-900 text-white py-6 text-center">
+      <footer className="bg-gray-900 text-fuchsia-300 py-6 text-center">
         <p>&copy; 2024 Victor Muthomi. All rights reserved.</p>
       </footer>
     </>
