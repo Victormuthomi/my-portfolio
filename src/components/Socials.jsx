79e132d8350fa5a7e8e119544fe3b90a92a43c3b
muthomi100 <techeasesolns@gmail.com>
2025-02-21 12:34:57 +0300

    change x account.

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -21,7 +21,7 @@ const Socials = () => {
           <Link to="https://github.com/Victormuthomi">
             <FaGithub className="text-5xl text-white hover:text-fuchsia-400 animate-pulse" />
           </Link>
-          <Link to="https://x.com/kiba_nga">
+          <Link to="https://x.com/Victormuthomim">
             <FaXTwitter className="text-white text-5xl hover:text-fuchsia-400 animate-pulse" />
           </Link>
 
