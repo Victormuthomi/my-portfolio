5fdea746dccc0f22e69627071026673690e88374
Victormuthomi <victormuthomi100@gmail.com>
2025-06-17 11:54:24 +0300

    Modify about line to be only visible on md screen and above.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,19 +3,22 @@ import { FaUserGear } from "react-icons/fa6";
 const About = () => {
   return (
     <>
-      <div className="rounded-full border-2 border-fuchsia-300"></div>
+      {/* Full-width line, only visible on md and above */}
+      <div className="hidden md:block w-full border-t-4 border-fuchsia-300"></div>
+
       <div className="flex justify-center items-center">
         <section
           className="max-w-7xl md:py-6 py-10 h-auto mt-12 mb-0 w-full px-4 md:w-[950px] max-w-[90%]"
           id="about"
         >
-          <div className="flex justify-center items-center underline decoration-fuchsia-300 hover:decoration-fuchsia-500 hover:animate-pulse">
-            <h2 className="text-5xl font-bold text-center text-fuchsia-500 mb-6">
+          <div className="flex justify-center items-center">
+            <h2 className="text-2xl sm:text-3xl md:text-5xl font-bold text-center text-fuchsia-500 mb-6 whitespace-nowrap">
               ABOUT ME
             </h2>
-            <FaUserGear className="text-5xl ml-4 mb-6 text-white animate-spin slow-spin " />
+            <FaUserGear className="text-3xl sm:text-4xl md:text-5xl ml-4 mb-6 text-white animate-spin slow-spin" />
           </div>
-          <p className="md:text-2xl text-lg text-center md:text-justify text-slate-200 leading-relaxed">
+
+          <p className="text-lg sm:text-xl md:text-2xl text-center md:text-justify text-slate-200 leading-relaxed">
             I'm a Full-Stack Developer passionate about building real-world
             solutions. I work with MongoDB, Express.js, React.js, Node.js,
             Python/Django, Go, Gin, PostgreSQL, and DevOps to create scalable,
